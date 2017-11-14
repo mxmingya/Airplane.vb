e595ac07a7f4f6aaf2dcc93b714fb3c83b4c919c
Ming <mxmingya@gmail.com>
2017-11-14 16:19:30 -0600

    Initial commit

---
+++ Airplane.vb/Form1.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmAirplane
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.NameBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LocationBox = New System.Windows.Forms.TextBox()
+        Me.AddPassengerButton = New System.Windows.Forms.Button()
+        Me.DeletePassengerButton = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SeatListBox = New System.Windows.Forms.ListBox()
+        Me.WaitListBox = New System.Windows.Forms.ListBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.NameBox.Location = New System.Drawing.Point(88, 32)
+        Me.NameBox.Name = "TextBox1"
+        Me.NameBox.Size = New System.Drawing.Size(100, 20)
+        Me.NameBox.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.Label1.Location = New System.Drawing.Point(33, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.Label2.Location = New System.Drawing.Point(294, 32)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Seat:"
+        '
+        'TextBox3
+        '
+        Me.LocationBox.Location = New System.Drawing.Point(341, 32)
+        Me.LocationBox.Name = "TextBox3"
+        Me.LocationBox.Size = New System.Drawing.Size(43, 20)
+        Me.LocationBox.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.AddPassengerButton.Location = New System.Drawing.Point(49, 88)
+        Me.AddPassengerButton.Name = "Button1"
+        Me.AddPassengerButton.Size = New System.Drawing.Size(93, 47)
+        Me.AddPassengerButton.TabIndex = 5
+        Me.AddPassengerButton.Text = "Add Passenger"
+        Me.AddPassengerButton.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.DeletePassengerButton.Location = New System.Drawing.Point(49, 174)
+        Me.DeletePassengerButton.Name = "Button2"
+        Me.DeletePassengerButton.Size = New System.Drawing.Size(93, 43)
+        Me.DeletePassengerButton.TabIndex = 6
+        Me.DeletePassengerButton.Text = "Delete Passenger"
+        Me.DeletePassengerButton.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(296, 55)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(147, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "(1A, 1B, 1C, 1D, 2A, ... , 10D)"
+        '
+        'ListBox1
+        '
+        Me.SeatListBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.25!)
+        Me.SeatListBox.FormattingEnabled = True
+        Me.SeatListBox.ItemHeight = 29
+        Me.SeatListBox.Location = New System.Drawing.Point(297, 88)
+        Me.SeatListBox.Name = "ListBox1"
+        Me.SeatListBox.Size = New System.Drawing.Size(140, 294)
+        Me.SeatListBox.TabIndex = 11
+        '
+        'ListBox2
+        '
+        Me.WaitListBox.FormattingEnabled = True
+        Me.WaitListBox.Location = New System.Drawing.Point(299, 431)
+        Me.WaitListBox.Name = "ListBox2"
+        Me.WaitListBox.Size = New System.Drawing.Size(138, 134)
+        Me.WaitListBox.TabIndex = 12
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label4.Location = New System.Drawing.Point(344, 400)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 20)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "waitlist"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(474, 582)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.WaitListBox)
+        Me.Controls.Add(Me.SeatListBox)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.DeletePassengerButton)
+        Me.Controls.Add(Me.AddPassengerButton)
+        Me.Controls.Add(Me.LocationBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.NameBox)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents NameBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents LocationBox As System.Windows.Forms.TextBox
+    Friend WithEvents AddPassengerButton As System.Windows.Forms.Button
+    Friend WithEvents DeletePassengerButton As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents SeatListBox As System.Windows.Forms.ListBox
+    Friend WithEvents WaitListBox As System.Windows.Forms.ListBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ Airplane.vb/Form1.vb
@@ -0,0 +1,162 @@
+﻿Public Class FrmAirplane
+    ' global variables declared here
+    Dim Seat(9, 3) As String
+    Dim order As String = "ABCD"
+    Dim waitList As New List(Of String)()
+    'waitList contains the names of passengers who are waiting for a seat
+    Dim map As New System.Collections.Generic.Dictionary(Of String, String)
+    ' map is a collection maps the passengers' names to their desire seats 
+
+
+    'EVENT SUBROUTINES
+    Private Sub FrmAirplane_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DisplaySeatingChart()
+    End Sub
+
+    Private Sub AddPassengerButton_Click(sender As Object, e As EventArgs) Handles AddPassengerButton.Click
+        Dim name As String = NameBox.Text
+        Dim location As String = LocationBox.Text
+        Dim parsedLocation = parseLocation(location)
+        Dim row As String = parsedLocation(0)
+        Dim col As String = parsedLocation(1)
+
+        'detect invalid input
+        If row > 9 Or row < 0 Or col > 3 Or col < 0 Or name = "" Then
+            MsgBox("invalid input, please choose a valid seat or type a valid name")
+        Else
+            addPassenger(location, name)
+            DisplaySeatingChart()
+        End If
+
+        NameBox.Text = ""
+        LocationBox.Text = ""
+
+    End Sub
+
+    Private Sub DeletePassengerButton_Click(sender As Object, e As EventArgs) Handles DeletePassengerButton.Click
+        If (NameBox.Text = "") Then
+            MsgBox("Please type a valid name")
+        Else
+            removePassenger(NameBox.Text)
+        End If
+        DisplaySeatingChart()
+    End Sub
+
+    'UTILITY FUNCTIONS AND SUBROUTINES
+    Private Sub DisplaySeatingChart()
+        SeatListBox.Items.Clear()
+        For row As Integer = 0 To 9
+            Dim rowStr As String = ""
+            For col As Integer = 0 To 3
+                If Seat(row, col) = "" Then
+                    rowStr &= ". "
+                Else
+                    rowStr &= "X "
+                End If
+
+                If col = 1 Then
+                    rowStr &= " "
+                End If
+            Next
+            SeatListBox.Items.Add(rowStr)
+        Next
+    End Sub
+
+    Private Function parseLocation(Location As String)
+        Dim row As Integer
+        Dim col As Integer
+        If (Location.Length = 3) Then
+            ' handle cases like 10A
+            row = CInt(Location.Substring(0, 2)) - 1
+            Dim s As String = Location.Substring(2, 1)
+            col = order.IndexOf(s)
+        Else
+            row = CInt(Location.Substring(0, 1)) - 1
+            Dim s As String = Location.Substring(1, 1)
+            col = order.IndexOf(s)
+        End If
+        Return {row, col}
+    End Function
+
+    Private Sub addPassenger(location As String, name As String)
+        'Dim strs As String = location.ToCharArray
+        Dim locations() As Integer = parseLocation(location)
+        Dim row = locations(0)
+        Dim col = locations(1)
+
+        'if the requested seat is currently available
+        If Seat(row, col) = "" Then
+
+            If map.ContainsKey(name) Then
+                Dim preLocation = parseLocation(map.Item(name))
+                Dim preRow As Integer = preLocation(0)
+                Dim preCol As Integer = preLocation(1)
+                map.Remove(name)
+                Seat(preRow, preCol) = ""
+            End If
+            map.Add(name, location)
+            Seat(row, col) = name
+
+            'selected seat was took but there are other seats available
+        ElseIf spotAvailable() Then
+            MsgBox("Selected Seat is occupied, please choose a difference one")
+
+            'waitlist is not full and there aint seats available
+        ElseIf Not spotAvailable() And waitList.Count < 9 Then
+            waitList.Add(name)
+            displayWaitList()
+
+            'no seats available and the waitlist is full
+        Else
+            MsgBox("Sorry we have all seats sold out and wait list is full. Please check later for available seats")
+        End If
+    End Sub
+
+    Private Function spotAvailable()
+        ' check if there are any seats available in the flight, if so, return True, otherwise return False
+        For row As Integer = 0 To 9
+            For col As Integer = 0 To 3
+                If Seat(row, col) = "" Then
+                    Return True
+                End If
+            Next
+        Next
+        Return False
+    End Function
+
+    Private Sub removePassenger(name As String)
+        'delete the passenger from seat
+        If map.ContainsKey(name) Then
+            Dim location = map(name)
+            Dim parsedLocation = parseLocation(location)
+            Dim row As Integer = parsedLocation(0)
+            Dim col As Integer = parsedLocation(1)
+            map.Remove(name)
+
+            'if there are people in the wait list, give the new available seat to the waiting passenger
+            'otherwise mark the seat as avaiable
+            If (waitList.Count > 0) Then
+                Dim firstPassenger As String = waitList.First
+                waitList.Remove(firstPassenger)
+                map.Add(firstPassenger, location)
+            Else
+                Seat(row, col) = ""
+            End If
+            DisplaySeatingChart()
+
+            'delete the passenger from the wait list
+        ElseIf waitList.Contains(name) Then
+            waitList.Remove(name)
+            displayWaitList()
+        Else
+            MsgBox("Cannot find passenger " & name & " to remove, please check again")
+        End If
+
+    End Sub
+
+    Private Sub displayWaitList()
+        For Each n As String In waitList
+            WaitListBox.Items.Add(n)
+        Next
+    End Sub
+End Class
